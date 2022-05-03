affcc00d77b1784ed2e5379887518ceba86a88bc
sai kumar dontham <sai524470@gmail.com>
2022-05-03 23:58:05 +0530

    dreamFuture

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxPsvfhJALD-KTHRCgk2gSlDLzZLj22cU",
-  authDomain: "recruitment-76fb3.firebaseapp.com",
-  projectId: "recruitment-76fb3",
-  storageBucket: "recruitment-76fb3.appspot.com",
-  messagingSenderId: "320231127005",
-  appId: "1:320231127005:web:651a47ab75039bba976834",
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STRORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
