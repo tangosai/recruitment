4ed13a48dfc2ea6395f073949b5fc3e8819db8dd
sai kumar dontham <sai524470@gmail.com>
2022-05-04 00:03:17 +0530

    reset

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KE,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STRORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAxPsvfhJALD-KTHRCgk2gSlDLzZLj22cU",
+  authDomain: "recruitment-76fb3.firebaseapp.com",
+  projectId: "recruitment-76fb3",
+  storageBucket: "recruitment-76fb3.appspot.com",
+  messagingSenderId: "320231127005",
+  appId: "1:320231127005:web:651a47ab75039bba976834",
 };
 
 // Initialize Firebase
