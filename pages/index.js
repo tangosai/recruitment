b8a19a9f9d5f895bc5a385209086547d1dc6df53
sai kumar dontham <sai524470@gmail.com>
2022-05-04 00:14:50 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Layouts from "@/components/Layouts";
-import PopularJobs from "@/components/PopularJobs";
-import Slider from "@/components/Slider";
+import Layouts from "..//components/Layouts";
+import PopularJobs from "../components/PopularJobs";
+import Slider from "../components/Slider";
 
 export default function Home() {
   return (
